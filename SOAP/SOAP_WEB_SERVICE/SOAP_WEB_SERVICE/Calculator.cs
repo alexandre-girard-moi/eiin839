4d0199b4309b47a681572936f3804dd3a551dafc
Alex <alexandre.gi@live.fr>
2021-03-10 15:46:42 +0100

    Soap

---
+++ SOAP/SOAP_WEB_SERVICE/SOAP_WEB_SERVICE/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceReference1;
+
+namespace SOAP_WEB_SERVICE
+{
+    class Calculator : CalculatorSoap
+    {
+        public Task<int> AddAsync(int intA, int intB)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SubtractAsync(int intA, int intB)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> MultiplyAsync(int intA, int intB)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DivideAsync(int intA, int intB)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SOAP/SOAP_WEB_SERVICE/SOAP_WEB_SERVICE/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SOAP_WEB_SERVICE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
